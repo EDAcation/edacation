23c4c8b3f5407f0da7c719d8160dd56c2eec28fd
Mike A. <git@mikealmel.ooo>
2025-09-30 21:45:15 +0200

    Add flasher

--- src/project/index.ts
+++ src/project/index.ts
@@ -1,5 +1,6 @@
 export * from './configuration.js';
 export * from './devices.js';
+export * from './flasher.js';
 export * from './groups.js';
 export * from './iverilog.js';
 export * from './nextpnr.js';
