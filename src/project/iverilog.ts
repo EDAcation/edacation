df7ed72707065823724c0c4e9dd2d661c676fe6d
Mike A. <git@mikealmel.ooo>
2025-10-23 16:53:17 +0200

    refactor: remove `generateIVerilogWorkerOptions`

--- src/project/iverilog.ts
+++ src/project/iverilog.ts
@@ -4,8 +4,8 @@ import {parseArgs} from 'string-args-parser';
 import {FILE_EXTENSIONS_VERILOG} from '../util.js';
 
 import type {IVerilogOptions, ProjectConfiguration, WorkerOptions, WorkerStep} from './configuration.js';
-import type {Project, ProjectInputFile} from './project.js';
-import {getCombined, getDefaultOptions, getOptions, getTarget, getTargetFile} from './target.js';
+import type {Project} from './project.js';
+import {getCombined, getOptions, getTarget, getTargetFile} from './target.js';
 
 // Empty for now
 export type IVerilogStep = WorkerStep;
@@ -16,90 +16,85 @@ const DEFAULT_OPTIONS: IVerilogOptions = {
     testbenchFile: undefined
 };
 
-export const getIVerilogDefaultOptions = (configuration: ProjectConfiguration): IVerilogOptions =>
-    getDefaultOptions(configuration, 'iverilog', DEFAULT_OPTIONS);
+export const parseIVerilogArguments = (args: string[]) => args.flatMap((arg) => parseArgs(arg));
 
 export const getIVerilogOptions = (configuration: ProjectConfiguration, targetId: string): IVerilogOptions =>
     getOptions(configuration, targetId, 'iverilog', DEFAULT_OPTIONS);
 
-export const generateIVerilogWorkerOptions = (
-    configuration: ProjectConfiguration,
-    projectInputFiles: ProjectInputFile[],
+export const getIVerilogWorkerOptions = (
+    project: Project,
     targetId: string
 ): IVerilogWorkerOptions => {
+    const configuration = project.getConfiguration();
     const target = getTarget(configuration, targetId);
     const options = getIVerilogOptions(configuration, targetId);
 
-    const files = projectInputFiles.filter(
+    // Input files
+    const designFiles = project.getInputFiles().filter(
         (inputFile) =>
             inputFile.type === 'design' && FILE_EXTENSIONS_VERILOG.includes(path.extname(inputFile.path).substring(1))
-    );
-    const designFiles = files.map((file) => file.path);
+    ).map(file => file.path);
+
     let testbenchFile = options.testbenchFile;
     if (!testbenchFile) {
         // Auto-select from testbench input files
-        const allTestbenches = projectInputFiles.filter((file) => file.type === 'testbench');
+        const allTestbenches = project.getInputFiles().filter((file) => file.type === 'testbench');
         if (allTestbenches.length === 0)
             throw new Error('Could not auto-select testbench file: no input files marked as such');
         testbenchFile = allTestbenches[0].path;
     }
-    const inputFiles = designFiles.concat([testbenchFile]);
 
-    const compiledFile = getTargetFile(target, 'simulator.vvp');
-    const outputFiles: string[] = [compiledFile, `${path.parse(testbenchFile).name}.vcd`];
-
-    const compileArgs: string[] = [];
-    compileArgs.push('-o', compiledFile);
-    compileArgs.push(...designFiles);
-    compileArgs.push(testbenchFile);
-
-    const steps = [
-        {tool: 'iverilog', arguments: compileArgs},
-        {tool: 'vvp', arguments: [compiledFile]}
-    ];
-
-    return {
-        inputFiles,
-        outputFiles,
-        target,
-        options,
-        steps
-    };
-};
-
-export const parseIVerilogArguments = (args: string[]) => args.flatMap((arg) => parseArgs(arg));
-
-export const getIVerilogWorkerOptions = (project: Project, targetId: string): IVerilogWorkerOptions => {
-    const generated = generateIVerilogWorkerOptions(project.getConfiguration(), project.getInputFiles(), targetId);
-
+    const generatedInputFiles = designFiles.concat([testbenchFile]);
     const inputFiles = getCombined(
         project.getConfiguration(),
         targetId,
         'iverilog',
         'inputFiles',
-        generated.inputFiles
+        generatedInputFiles
     ).filter((f) => !!f);
+
+    // Output files
+    const compiledFile = getTargetFile(target, 'simulator.vvp');
+    const generatedOutputFiles: string[] = [compiledFile, `${path.parse(testbenchFile).name}.vcd`];
     const outputFiles = getCombined(
         project.getConfiguration(),
         targetId,
         'iverilog',
         'outputFiles',
-        generated.outputFiles
+        generatedOutputFiles
     ).filter((f) => !!f);
 
-    const target = generated.target;
-    const options = generated.options;
-    const steps = generated.steps.map((step) => {
-        const tool = step.tool;
-        const args = getCombined(project.getConfiguration(), targetId, 'iverilog', 'arguments', step.arguments, parseIVerilogArguments);
-        return {tool, arguments: args};
-    });
+    // Args
+    const generatedCompileArgs: string[] = [
+        '-o', compiledFile,
+        ...designFiles,
+        testbenchFile,
+    ];
+    const compileArgs = getCombined(
+        project.getConfiguration(),
+        targetId,
+        'iverilog',
+        'arguments',
+        generatedCompileArgs,
+        parseIVerilogArguments
+    );
 
     return {
         inputFiles,
         outputFiles,
         target,
         options,
-        steps
+        steps: [
+            {
+                id: 'iverilog',
+                tool: 'iverilog',
+                arguments: compileArgs
+            },
+            {
+                id: 'vvp',
+                tool: 'vvp',
+                arguments: [compiledFile]
+            }
+        ]
     };
 };
